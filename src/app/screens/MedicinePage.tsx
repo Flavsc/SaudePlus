e8c3a8d34ad4b22645c4080fc6d9f865f345524e
Gustavo Ara <gustavo.ara2005.p@gmail.com>
2024-05-24 10:49:24 -0300

    Medicine Page Chages

--- src/app/screens/MedicinePage.tsx
+++ src/app/screens/MedicinePage.tsx
@@ -1,39 +1,49 @@
 import SPlus from "@assets/images/YELogo";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import RegularButton from "components/Buttons/RegularButton";
-import CameraT from "components/Camera/Camera";
 import GridView from "components/Grids/GridView";
 import MainContainer from "components/containers/MainContainer";
-import SmallContainer from "components/containers/SmallContainer";
 import BigText from "components/texts/BigText";
-import RegularText from "components/texts/RegularText";
 import React from "react";
-import { View } from "react-native";
-import { Button, SegmentedButtons } from "react-native-paper";
+import { SafeAreaView, StyleSheet, View, Text} from "react-native";
+import { Button, Portal, Provider} from "react-native-paper";
 
 export default function MainPage({ navigation }: { navigation: any }) {
     const [value, setValue] = React.useState("");
     return (
         <MainContainer>
-            <BigText style={{ marginBottom: 10 }}>Remédios Salvos</BigText>
-            <SmallContainer style={{ flexDirection: "row", flexWrap: "wrap" }}>
-                <SegmentedButtons
-                    value={value}
-                    onValueChange={setValue}
-                    buttons={[
-                        {
-                            value: "plus",
-                            label: "Adicionar"
-                        },
-                        {
-                            value: "pencil",
-                            label: "Editar"
-                        },
-                        { value: "delete", label: "Excluir" }
-                    ]}
-                />
-            </SmallContainer>
+            <BigText style={{ marginTop: 10 }}>Remédios Salvos</BigText>
+
+            <Provider>
+                <SafeAreaView style={styles.container}>
+                    <View style={styles.buttonContainer}>
+                        <Button 
+                            mode="contained"
+                            style={{ backgroundColor: "#739986" }}
+                            onPress={() => 
+                                <Portal>
+                                    <Text>This is rendered at a different place</Text>
+                                </Portal>}
+                        >
+                            Press Me
+                        </Button>
+                    </View>
+                </SafeAreaView>
+            </Provider>
         </MainContainer>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'flex-end', // Alinha o botão na parte inferior
+      alignItems: 'flex-end', // Alinha o botão na direita
+      
+    },
+    buttonContainer: {
+      position: 'absolute',
+      bottom: 16,
+      left: 70,
+    },
+  });
